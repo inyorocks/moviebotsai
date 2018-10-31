275d153692914b1e9666a61e0e346f7311676e84
inyorocks <44603843+inyorocks@users.noreply.github.com>
2018-10-31 14:38:59 +1000

    bug fix
    bug fix

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var app = require('express').createServer();
 app.post('/getName',function(req,res) {
- if(res.body.result.parameters['name']) {
+
 	 res.setHeader('Content-Type', 'application/json');
                     res.send(JSON.stringify({
                         "speech" : "Success called Node js  ? ",
                         "displayText" : "Success called Node js ? "
                     }));
- }
+ 
 });
 
 app.listen(process.env.PORT || 3000,function(){
