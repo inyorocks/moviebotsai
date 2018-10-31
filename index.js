e585398bacb35ae9f4ccc34e00b0daf019e0479c
inyorocks <44603843+inyorocks@users.noreply.github.com>
2018-10-31 15:15:02 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,20 +2,12 @@ var app = require('express').createServer();
 app.post('/getName',function(req,res) {
 
 var userName = req.body.queryResult.parameters['name'];
-	    if(userName) {
+	  
 	 res.setHeader('Content-Type', 'application/json');
                     res.send(JSON.stringify({
                         "fulfillmentText" : "Success in calling Node js. Good job" + userName        
                     }));
-			}else{
-						 res.setHeader('Content-Type', 'application/json');
-                    res.send(JSON.stringify({
-                        "fulfillmentText" : "Success in calling Node js"
-        
-                    }));
-		    }				
-					
- 
+	
 });
 app.listen(process.env.PORT || 3000,function(){
 	console.log("listening on 3000");
