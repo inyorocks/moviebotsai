b1ec17439170e354df3621669d3ba47033b52442
inyorocks <44603843+inyorocks@users.noreply.github.com>
2018-10-31 15:04:13 +1000

    adding name to the response
    adding name to the response

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var app = require('express').createServer();
 app.post('/getName',function(req,res) {
 
+	    if(request.body.result.parameters['name']) {
 	 res.setHeader('Content-Type', 'application/json');
+                    res.send(JSON.stringify({
+                        "fulfillmentText" : "Success in calling Node js. Good job" + request.body.result.parameters['name']
+        
+                    }));
+			}else{
+						 res.setHeader('Content-Type', 'application/json');
                     res.send(JSON.stringify({
                         "fulfillmentText" : "Success in calling Node js"
         
                     }));
+		    }				
+					
  
 });
 app.listen(process.env.PORT || 3000,function(){
