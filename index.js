0ff242e3dbeb59e83592b710539088e945181023
inyorocks <44603843+inyorocks@users.noreply.github.com>
2018-10-31 21:31:44 +1000

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ app.post('/getName',function(req,res) {
 	const data = req.body;
 	 res.setHeader('Content-Type', 'application/json');
                     res.send(JSON.stringify({
-                        "fulfillmentText" : req.body
+                        "fulfillmentText" : req.body.queryResult.parameters
                     }));
  }); 
 app.listen(process.env.PORT || 3000,function(){
