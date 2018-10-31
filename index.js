c5b8f247c0c14af050285b49ff12afe143f4edee
inyorocks <44603843+inyorocks@users.noreply.github.com>
2018-10-31 18:48:27 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 var app = require('express').createServer();
 app.post('/getName',function(req,res) {
-
 	 res.setHeader('Content-Type', 'application/json');
                     res.send(JSON.stringify({
-                        "fulfillmentText" : "Success in calling Node js"
-        
+                        "fulfillmentText" : "Success in calling Node js"   
                     }));
- 
-});
+ });
 app.listen(process.env.PORT || 3000,function(){
 	console.log("listening on 3000");
 });
