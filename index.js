6be88d9491480b1603916ef28d024c0b07845fb6
inyorocks <44603843+inyorocks@users.noreply.github.com>
2018-11-01 21:11:57 +1000

    Update index.js

--- index.js
+++ index.js
@@ -5,34 +5,28 @@ var server = http.createServer(app);
 // configure the app to use bodyParser()
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.post('/getName',function(request,response) {
-	var val = request.body.queryResult.parameters['GIVEN_NAME'];
-	console.log("Value of 3");
-	var req = unirest("GET", "https://ehpdev2.appiancloud.com/suite/webapi/permitSearch?permitRef_txt=EA0003548");
+app.post('/getName', function(request, response) {
+            var val = request.body.queryResult.parameters['GIVEN_NAME'];
+            console.log("Value of 3");
+            var req = unirest("GET", "https://ehpdev2.appiancloud.com/suite/webapi/permitSearch?permitRef_txt=EA0003548");
             req.query({
                 "page": "1",
                 "language": "en-US",
-                "username": "sai.ramesh@ehp.qld.gov.au",
-		 "password" : "Hanuman.01"   
+                "username": "",
+                "password": ""
             });
             req.send("{}");
-	console.log("Value of 4");
+            console.log("Value of 4");
             req.end(function(res) {
-                if(res.error) {
-                   response.setHeader('Content-Type', 'application/json');
+                if (res.error) {
+                    response.setHeader('Content-Type', 'application/json');
                     response.send(JSON.stringify({
-                        "fulfillmentText" : "Error Mate"
+                        "fulfillmentText": "Error Mate"
                     }));
-				}else{
-	
-	 response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "fulfillmentText" : "Thanks for reaching out " + val
-                    }));
-			}
-	
- });
-}
-app.listen(process.env.PORT || 3000,function(){
-	console.log("listening on 3000");
-});
+                } 
+
+            });
+        }
+        app.listen(process.env.PORT || 3000, function() {
+            console.log("listening on 3000");
+        });
