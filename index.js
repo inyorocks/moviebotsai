6b856227505ef4cc4be9bff79668bdd38cd56eb9
inyorocks <44603843+inyorocks@users.noreply.github.com>
2018-11-01 21:15:49 +1000

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ var server = http.createServer(app);
 // configure the app to use bodyParser()
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.post('/getName', function(request, response) {
+app.post('/getName', function(request, response) 
+        {
             var val = request.body.queryResult.parameters['GIVEN_NAME'];
             console.log("Value of 3");
             var req = unirest("GET", "https://ehpdev2.appiancloud.com/suite/webapi/permitSearch?permitRef_txt=EA0003548");
@@ -18,12 +19,7 @@ app.post('/getName', function(request, response) {
             req.send("{}");
             console.log("Value of 4");
             req.end(function(res) {
-                if (res.error) {
-                    response.setHeader('Content-Type', 'application/json');
-                    response.send(JSON.stringify({
-                        "fulfillmentText": "Error Mate"
-                    }));
-                } 
+               
 
             });
         }
