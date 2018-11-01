3d7709966539c464c1cb20297cf301692843f3b3
inyorocks <44603843+inyorocks@users.noreply.github.com>
2018-11-01 18:53:17 +1000

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,29 @@ app.use(bodyParser.json());
 app.post('/getName',function(request,response) {
 	var val = request.body.queryResult.parameters['GIVEN_NAME'];
 	console.log("Value of 3");
+	ar request = require('request');
+
+	var url = 'https://ehpdev2.appiancloud.com/suite/webapi/permitSearch?permitRef_txt=val';
+	var user = 'sai.ramesh@ehp.qld.gov.au';
+	var pass = 'Hanuman.01';
+
+	// Save these for future requests
+	var userId;
+	var authToken;
+
+	// Use POST instead of GET
+	request.post(
+	  {
+	    uri: url,
+	    // I'm using form because it matches the urlEncoding behaviour expected by `restivus`
+	    form: { username: user, password: pass }
+	  },
+	  function(err, httpResponse, body) {
+	    if (err) {
+	      return console.error('post failed:', err);
+	    }
+        )
+	
 	response.setHeader('Content-Type', 'application/json');
                     response.send(JSON.stringify({
                         "fulfillmentText" : "Thanks for reaching out " + val
