a632e56ac7e5c59cc98d71d42a8254d13f149d88
inyorocks <44603843+inyorocks@users.noreply.github.com>
2018-11-01 18:20:48 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-console.log("Value of 0");
 var express = require('express');
-console.log("Value of a");
 var http = require('http');
-console.log("Value of b");
 var app = express();
-console.log("Value of c");
 var server = http.createServer(app);
-console.log("Value of 1");
 // configure the app to use bodyParser()
 var bodyParser = require('body-parser');
-console.log("Value of 1.1");
 app.use(bodyParser.json());
-console.log("Value of 2");
 app.post('/getName',function(req,res) {
 	var val = req.body.queryResult.parameters['GIVEN_NAME'];
 	console.log("Value of 3");
