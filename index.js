e8dc0f4060d9c8a6f00dcb1b53e7e49449881dd2
inyorocks <44603843+inyorocks@users.noreply.github.com>
2018-11-01 10:14:02 +1000

    Update index.js

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ var app = require('express').createServer();
 // configure the app to use bodyParser()
 app.use(bodyParser.json());
 app.post('/getName',function(req,res) {
-	var val = req.queryResult.parameters['name']
+	var val = req.queryResult.parameters['name'];
+	console.log("Value of :" val);
 	 res.setHeader('Content-Type', 'application/json');
                     res.send(JSON.stringify({
                         "fulfillmentText" : val
                     }));
  }); 
-console.log("Value of :" val)
+
 app.listen(process.env.PORT || 3000,function(){
 	console.log("listening on 3000");
 });
