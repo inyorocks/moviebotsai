ebcc5de019db0e3d7617e2d4d99ddab29efffd4d
inyorocks <44603843+inyorocks@users.noreply.github.com>
2018-11-01 22:29:27 +1000

    Update index.js

--- index.js
+++ index.js
@@ -11,8 +11,10 @@ app.post('/getName', function(request, response) {
             var val = request.body.queryResult.parameters['GIVEN_NAME'];
             console.log("Value of 3");
             var https = require('https');
+            console.log("Value of 4");
             var username = 'sai.ramesh@ehp.qld.gov.au';
             var password = 'Hanuman.01';
+            console.log("Value of 5");
             var options = {
               url: 'https://ehpdev2.appiancloud.com/suite/webapi/permitSearch?permitRef_txt=EA0003548',
               auth: {
@@ -20,6 +22,7 @@ app.post('/getName', function(request, response) {
                 password: password
               }
             }
+            console.log("Value of 6");
             var req = https.request(options, function(res) {
                 if (res.error) {
                     response.setHeader('Content-Type', 'application/json');
