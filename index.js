8c12f96dcda81b45569c62f09d8ff2c475616a50
inyorocks <44603843+inyorocks@users.noreply.github.com>
2018-11-01 21:24:52 +1000

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ var server = http.createServer(app);
 // configure the app to use bodyParser()
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
-app.post('/getName', function(request, response) 
-        {
+app.post('/getName', function(request, response) {
             var val = request.body.queryResult.parameters['GIVEN_NAME'];
             console.log("Value of 3");
             var req = unirest("GET", "https://ehpdev2.appiancloud.com/suite/webapi/permitSearch?permitRef_txt=EA0003548");
@@ -16,8 +15,24 @@ app.post('/getName', function(request, response)
                 "username": "",
                 "password": ""
             });
-            
-        }
+            req.send("{}");
+            console.log("Value of 4");
+            req.end(function(res) {
+                if (res.error) {
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "fulfillmentText": "Error Mate"
+                    }));
+                } else {
+
+                    response.setHeader('Content-Type', 'application/json');
+                    response.send(JSON.stringify({
+                        "fulfillmentText": "Thanks for reaching out " + val
+                    }));
+                }
+
+            });
+        });
         app.listen(process.env.PORT || 3000, function() {
             console.log("listening on 3000");
         });
